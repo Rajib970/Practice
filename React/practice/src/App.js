e54cf1a7d49a72f4fcc1ea01abd187c60f05fe95
Rajib Shaw <rajibshaw582@gmail.com>
2025-01-03 18:59:52 +0530

    Added React Router Concept

--- React/practice/src/App.js
+++ React/practice/src/App.js
@@ -5,6 +5,15 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   Link
+} from "react-router-dom";
+
+
+
 
 function App() {
    const [mode, setMode] = useState('light');
@@ -26,13 +35,13 @@ function App() {
          document.body.style.backgroundColor = "grey";
          showAlert("Dark Mode Has Been Enabled", "success");
          document.title = 'TextUtils- Dark Mode';
-         setInterval(() =>{
+         setInterval(() => {
             document.title = 'TextUtils is amazing';
-         },1500);
+         }, 1500);
 
-         setInterval(()=>{
+         setInterval(() => {
             document.titlee = 'Install TextUtils Now';
-         },3000);
+         }, 3000);
       } else {
          setMode('light')
          document.body.style.backgroundColor = "white";
@@ -43,16 +52,20 @@ function App() {
    }
    return (
       <>
-         <Navbar title="TextUtils2" mode={mode} toggleMode={toggleMode} />
-         <Alert alert={alert} />
-         {/* <Navbar/> */}
-         <div className='container'>
-            <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />
-            {/* <About/> */}
+         <Router>
+            <Navbar title="TextUtils2" mode={mode} toggleMode={toggleMode} />
+            <Alert alert={alert} />
+            {/* <Navbar/> */}
+            <div className='container'>
+               <Routes>
+                  <Route exact path="/about" element={<About />}>
+                  </Route>
+                  <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />}>
 
-         </div>
-
-
+                  </Route>
+               </Routes>
+            </div>
+         </Router>
       </>
    );
 }

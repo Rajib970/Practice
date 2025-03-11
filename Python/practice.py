9aab6de8b6e0f5265d98baceae239ace347b64f1
Rajib Shaw <rajibshaw582@gmail.com>
2025-03-11 16:01:06 +0530

    Functions

--- Python/practice.py
+++ Python/practice.py
@@ -1,1 +1,26 @@
-print(1)
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First Number is Greater")
+    else:
+        print("Second NUmber is Greater") 
+
+def islesser(a,b):
+    pass        
+
+
+a = 9
+b = 7
+isGreater(a,b)
+calculateGmean(a,b)   
+
+c = 9
+d = 10
+
+isGreater(c,d)
+calculateGmean(c,d)
+
+

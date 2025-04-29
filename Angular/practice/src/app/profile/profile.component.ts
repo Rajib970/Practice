f6b03dd644861fbcc2b756f2f05e94cffb388c0e
Rajib Shaw <rajibshaw582@gmail.com>
2025-04-29 15:47:41 +0530

    Components and Custom Components

---
+++ Angular/practice/src/app/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'div',
+    // template: '<h1> Profile Component </h1>',
+    // styles:'h1{color:red}'
+    templateUrl: './profile.component.html',
+    styleUrl: './profile.component.css'
+    
+})
+export class ProfileComponent{
+
+}

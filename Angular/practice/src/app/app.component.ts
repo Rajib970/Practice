e514dca21847a2eb6f9db73c4fc576f2dcb67684
Rajib Shaw <rajibshaw582@gmail.com>
2025-06-03 18:03:39 +0530

    Two way Binding

--- Angular/practice/src/app/app.component.ts
+++ Angular/practice/src/app/app.component.ts
@@ -1,17 +1,15 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ProfileComponent } from './profile/profile.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  // users = ['Rajib', 'Ram', 'Shyam', 'Harry', 'Peter']
-  users = []
+  name = "Rajib"
 }
 

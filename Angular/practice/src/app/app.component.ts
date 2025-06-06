8a66f5ab1cb0613d41df652a01cce6b51df8e514
Rajib Shaw <rajibshaw582@gmail.com>
2025-06-06 17:06:45 +0530

    Directives

--- Angular/practice/src/app/app.component.ts
+++ Angular/practice/src/app/app.component.ts
@@ -1,24 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  bgColor = "red"
-
-  headingSizeBig = "100px"
-  headingSizeSmall = "30px"
-
-  zoom = true
-
-  UpdateFont() {
-    this.zoom = !this.zoom;
-  }
+  show = true
 }
 

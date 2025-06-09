024751bc9cff6b7940b2dc3c0da6260bcccfdb68
Rajib Shaw <rajibshaw582@gmail.com>
2025-06-09 16:02:16 +0530

    ngFor

--- Angular/practice/src/app/app.component.ts
+++ Angular/practice/src/app/app.component.ts
@@ -1,17 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, NgIf],
+  imports: [RouterOutlet, FormsModule, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  show = true
+  students = ["Anil", "Sidhu", "Sam", "Bruce", "Peter"]
+
+  studentsData = [
+    {
+      name: 'Rajib',
+      roll: '30',
+      email: 'rajib@gmail.com'
+    },
+    {
+      name: 'Sam',
+      roll: '33',
+      email: 'sam@gmail.com'
+    },
+    {
+      name: 'Jhon',
+      roll: '35',
+      email: 'jhon@gmail.com'
+    },
+    {
+      name: 'Karan',
+      roll: '40',
+      email: 'karan@gmail.com'
+    }, {
+      name: 'Peter',
+      roll: '50',
+      email: 'peter@gmail.com'
+    }
+  ]
 }
 

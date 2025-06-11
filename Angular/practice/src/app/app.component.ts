2e9be7cdbe83a49085b00c69b7c00e345009bdd1
Rajib Shaw <rajibshaw582@gmail.com>
2025-06-11 18:00:01 +0530

    NgSwitch Directive

--- Angular/practice/src/app/app.component.ts
+++ Angular/practice/src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, NgIf, NgFor],
+  imports: [RouterOutlet, FormsModule, NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  block = 0
+  color = ''
 
-  updateBlock() {
-    this.block++;
+  updateColor(color: string) {
+    this.color = color;
   }
 }
 

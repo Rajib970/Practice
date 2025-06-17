dfc8761f863345b033bd999ba361c82c7ab1732a
Rajib Shaw <rajibshaw582@gmail.com>
2025-06-17 14:38:38 +0530

    Routing

--- Angular/practice/src/app/app.component.ts
+++ Angular/practice/src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  color = ''
 
-  updateColor(color: string) {
-    this.color = color;
-  }
 }
 

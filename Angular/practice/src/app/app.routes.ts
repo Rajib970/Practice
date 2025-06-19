b96a92cef7f72b559c8634891d3a2c2fc03f0f12
Rajib Shaw <rajibshaw582@gmail.com>
2025-06-19 12:57:37 +0530

    404 Page

--- Angular/practice/src/app/app.routes.ts
+++ Angular/practice/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
 
 export const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'login', component: LoginComponent },
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: '**', component: Page404Component }
 ];

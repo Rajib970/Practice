ebdef48a78c4f21190aa5e4021f1febe4e2183af
Rajib Shaw <rajibshaw582@gmail.com>
2025-07-08 16:42:17 +0530

    Template Driven Form

--- Angular/practice/src/app/app.component.ts
+++ Angular/practice/src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, ReactiveFormsModule, NgIf, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
 
+  userDetail: any;
+
+  addDetails(val: NgForm) {
+    this.userDetail = val;
+  }
+
 }
+
 

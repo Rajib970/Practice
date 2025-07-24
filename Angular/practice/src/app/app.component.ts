7704cd5c69495878eb024a019ebdfd2715c1dd6b
Rajib Shaw <rajibshaw582@gmail.com>
2025-07-24 16:17:01 +0530

    Pipes

--- Angular/practice/src/app/app.component.ts
+++ Angular/practice/src/app/app.component.ts
@@ -2,25 +2,26 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { UserComponent } from './user/user.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, HeaderComponent, ReactiveFormsModule, NgIf, FormsModule, UserComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, ReactiveFormsModule, NgIf, FormsModule, UserComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
+  title = "Code Step By Step"
+  name = "RAJIB"
 
-  userlist: undefined | string;
+  date = new Date();
 
-  handleUsers(users: string) {
-    console.log(users);
-    this.userlist = users;
-  }
+  amount = 10;
+
+
 }
 
 

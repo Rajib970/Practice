1238c748181a13462deef9dbfe5d1aefffb2d921
Rajib Shaw <rajibshaw582@gmail.com>
2025-08-14 13:13:50 +0530

    Query parameters

--- FastApi/practice/books.py
+++ FastApi/practice/books.py
@@ -12,22 +12,21 @@ BOOKS = [
 ]
 
 
-@app.get("/books") # url: http://127.0.0.1:8000/api-endpoint
-async def read_all_books():
-    return BOOKS
-
-@app.get("/books/mybook") 
-async def read_all_books(mybook):
-    return {"book_title":"My Favourite Book"}
-
-@app.get("/books/{dynamic_param}")
-async def read_all_books(dynamic_param:str): 
-    return {"dynamic_param":dynamic_param}
-
-@app.get("/book/{book_title}")
-async def read_book(book_title:str):
+@app.get("/books/") # url: http://127.0.0.1:8000/api-endpoint
+async def read_category_by_query(category:str):
+    books_to_return = []
     for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
-            return book
+        if book.get('category').casefold() == category.casefold():
+             books_to_return.append(book)
 
+    return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author:str, category:str):
+    books_to_return = []
+    for book in BOOKS: 
+        if book.get('author').casefold() == book_author.casefold() and \
+           book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return            
 
